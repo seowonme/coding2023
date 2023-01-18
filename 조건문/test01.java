712133ec6a5cb0625a85373563bd1dceb8727e44
seowonme <86937748+seowonme@users.noreply.github.com>
2023-01-18 22:43:22 +0900

    두 수 비교하기

---
+++ 조건문/test01.java
@@ -0,0 +1,23 @@
+package Á¶°Ç¹®;
+
+import java.util.Scanner;
+
+public class test01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if (a > b) {
+			System.out.println(">");
+		} else if (a == b) {
+			System.out.println("==");
+		} else if (a < b) {
+			System.out.println("<");
+		}
+
+	}
+
+}
